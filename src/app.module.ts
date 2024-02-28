e9e8952b1a26995c848d3b55a1c23d00d93081c1
Fernando Wabab <wababfernando@gmail.com>
2024-02-28 00:17:52 -0300

    Se instalan librerias, modificaciones dentro del main y se crea conexion a la base de datos con variables de entorno.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
