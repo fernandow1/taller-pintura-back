f3667fa5d50672eb16285f760873c51100a2a9dc
Fernando <wababfernando@gmail.com>
2024-04-24 10:20:06 -0300

    Se crea el filtro y se aplica en el app module.

---
+++ src/shared/filters/query-failed.filter.ts
@@ -0,0 +1,24 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpStatus,
+  Logger,
+} from '@nestjs/common';
+import { QueryFailedError } from 'typeorm';
+
+@Catch(QueryFailedError)
+export class QueryFailedFilter implements ExceptionFilter {
+  catch(exception: QueryFailedError, host: ArgumentsHost): Response {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+
+    Logger.error(
+      `Ha ocurrido un error en la Base de Datos. ${exception.message}`,
+    );
+
+    return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+      message: 'Ha ocurrido un error en la base de datos.',
+    });
+  }
+}
